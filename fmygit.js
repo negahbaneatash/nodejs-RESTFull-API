fef6ab3b61b228bdf796be8ae53d5c6413bfdb53
Abouzar <abouzar.azarpira@gmail.com>
2021-06-20 22:01:31 +0800

    my third real commit message

--- fmygit.js
+++ fmygit.js
@@ -1,7 +1,7 @@
 // package.json scripts // "mygit":"node handle-mygit"
 // in the command line // npm run mygit -- my real commit message
-//
-const { exec } = require("child_process");
+
+const { exec } = require("child_process"); // to execute another commandline task in a running process
 
 const moduleArgs = process.argv; // -->  [node.exe, handle-mygit.js, my, real, commit, message]
 moduleArgs.splice(0, 2); // it removes the first two items in the array -->  [my, real, commit, message]
@@ -9,8 +9,10 @@ const commitMessage = moduleArgs.join(" "); // joins the all array items as one 
 
 exec("git add .", () => {
   console.log("everything was added");
+  // we write it here to be executed after 'git add .'
   exec(`git commit -m "${commitMessage}"`, () => {
     console.log("everything was commited by this message:", commitMessage);
+    exec("git push");
   });
 });
 
